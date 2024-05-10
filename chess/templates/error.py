a245e534688571b983aaf7074dac455c42667693
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-05-10 10:27:51 +0200

    error template added

---
+++ chess/templates/error.py
@@ -0,0 +1,9 @@
+class ErrorTemplate:
+    """Template for the error."""
+
+    @classmethod
+    def not_implemenyted(cls, *args, **kwargs) -> None:
+
+        print("This feature is not implemented yet ! .")
+
+        return None
