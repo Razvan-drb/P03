995acf7138aeac6146722bb7c940e1a0ab4d33a8
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-05-31 11:10:57 +0200

    major update

--- chess/views/tournament.py
+++ chess/views/tournament.py
@@ -4,95 +4,118 @@ from chess.models.tournaments import Tournament
 from chess.templates.tournament import TournamentTemplate
 
 
-class TournamentManagementSystem:
-    """Handles tournament management operations."""
+class TournamentView:
+        # return "TournamentView.menu", data"""Handles tournament management operations."""
 
-    def __init__(self):
-        self.tournament = None
-
-    def menu(self):
+    @staticmethod
+    def menu(data={}):
         """Display the tournament management menu."""
-        while True:
-            choice = TournamentTemplate.menu()
 
-            if choice == "1":
-                tournament_data = TournamentTemplate.create()
-                self.create_tournament(
-                    tournament_data["name"],
-                    tournament_data["start_date"],
-                    tournament_data["end_date"],
-                    tournament_data["description"],
-                    tournament_data["location"],
-                )
-            elif choice == "2":
-                show_all = Tournament.read_all()
-                chosen_tournament_id = display_available_tournaments(show_all)
-                if chosen_tournament_id:
-                    selected_tournament = Tournament.read_one(chosen_tournament_id)
-                    if selected_tournament:
-                        self.tournament = selected_tournament
-                        self.launch_tournament()
-                    else:
-                        print("Tournament not found.")
-            elif choice == "3":
-                break
-            else:
-                print("Invalid choice. Please enter a number between 1 and 3.")
+        choice = TournamentTemplate.menu()
 
-    def create_tournament(self, name, start_date, end_date, description, location):
-        """Creates a new tournament."""
-        tournament_id = secrets.token_hex(2)
-        self.tournament = Tournament(
-            name,
-            start_date,
-            end_date,
-            description,
-            location,
-            tournament_id=tournament_id,
-        )
-        self.tournament.create()
-        self.auto_create_rounds()
+        if choice == "1":
+            # tournament_data = TournamentTemplate.create()
+            # self.create_tournament(
+            #     tournament_data["name"],
+            #     tournament_data["start_date"],
+            #     tournament_data["end_date"],
+            #     tournament_data["description"],
+            #     tournament_data["location"],
+            # )
+            return "TournamentView.create_tournament", data
 
-    def auto_create_rounds(self):
-        """Automatically creates rounds for the tournament."""
-        print("Automatically creating rounds...")
-        for round_number in range(1, self.tournament.N_ROUNDS + 1):
-            matches = []
-            self.tournament._add_round(round_number, matches)
-        print("Rounds created successfully.")
+        elif choice == "2":
 
-    def launch_tournament(self):
-        """Launches the tournament."""
-        if self.tournament:
-            try:
-                TournamentTemplate.launch()
-                self.tournament.update_status("In Progress")
-                print("Tournament launched successfully.")
-            except ValueError as e:
-                print(e)
+            # show_all = Tournament.read_all()
+            # chosen_tournament_id = display_available_tournaments(show_all)
+            # if chosen_tournament_id:
+            #     selected_tournament = Tournament.read_one(chosen_tournament_id)
+            #     if selected_tournament:
+            #         self.tournament = selected_tournament
+            #         self.launch_tournament()
+            #     else:
+            #         print("Tournament not found.")
+            return "TournamentView.list_all_tournaments", data
+
+        elif choice == "3":
+            # Select / Load a Specific Tournament
+            return "TournamentView.Select", data
+
         else:
-            print("No tournament available to launch.")
+            # is the possibility to go to main menu anticpicated ?
+        # return "TournamentView.menu", data        return "PlayerView.menu", data
 
-    def add_player(self):
+    @staticmethod
+    def create_tournament(data={}):
+        """Creates a new tournament."""
+
+        # call crete template
+        # create tournament in db 
+
+        # return "TournamentView.menu", data
+
+
+    # @staticmethod
+    # def auto_create_rounds(data={}):
+    #     """Automatically creates rounds for the tournament."""
+    #     print("Automatically creating rounds...")
+    #     for round_number in range(1, self.tournament.N_ROUNDS + 1):
+    #         matches = []
+    #         self.tournament._add_round(round_number, matches)
+        # return "TournamentView.menu", data#     print("Rounds created successfully.")
+
+    @staticmethod  
+    def select(data={}):
+        """ """
+
+        id = TournamentTemplate.select() # a coder 
+
+        # get the tournament from the db
+        # self.tournament = Tournament.read_one(id) 
+
+        data["tournament_id"] = id
+        # return "TournamentView.menu", data    # return "TournamentView.menu", data
+
+    @staticmethod
+    def launch_tournament(data={}):
+        """Launches the tournament."""
+
+
+        # load the tournament with tournament id in data["tournament_id"]
+        # TournamenView.launch_tournament(tournament_dict)
+        # do tournament update status 
+
+        # return "TournamentView.menu", data    # return "TournamentView.menu", data
+
+    @staticmethod
+    def add_player(data={}):
         """Add a player to the tournament."""
-        player_data = TournamentTemplate.add_player()
-        new_player = Player(
-            firstname=player_data["firstname"],
-            lastname=player_data["lastname"],
-            birthdate=player_data["birthdate"]
-        )
-        new_player.create()
-        print("Player added successfully.")
 
-    def create_new_round(self):
+        # load the tournament with tournament id in data["tournament_id"]
+        # TournamenView.add_player(tournament_dict)
+        # DO Objsct player T.add_player(player)...
+
+        # return "TournamentView.menu", data
+    # return "TournamentView.menu", data
+
+    @staticmethod
+    def list_all_tournaments(data={}):
+        """Lists all available tournaments."""
+        list_tournaments = Tournament.read_all()
+        # return "TournamentView.menu", data    display_available_tournaments(list_tournaments)
+
+    @staticmethod
+    def create_new_round(data={}):
         """Create a new round in the tournament."""
         if self.tournament:
             TournamentTemplate.new_round()
             self.auto_create_rounds()
         else:
-            print("No tournament available to create a new round.")
+            pass
+        # return "TournamentView.menu", data        print("No tournament available to create a new round.")
 
-    def display_rankings(self):
+    @staticmethod
+    def display_rankings(data={}):
         """Display rankings of the tournament."""
         if self.tournament:
             rankings = self.tournament.calculate_rankings()
@@ -100,26 +123,26 @@ class TournamentManagementSystem:
         else:
             print("No tournament available to display rankings.")
 
-    def list_all_tournaments(self):
-        """Lists all available tournaments."""
-        list_tournaments = Tournament.read_all()
-        display_available_tournaments(list_tournaments)
+    # return "TournamentView.menu", data
 
+    @staticmethod
     def get_current_round(self, tournament):
         """Get the current round for the given tournament."""
-        return tournament.current_round
+        # return "TournamentView.menu", data    return tournament.current_round
 
-    def _next_round(self):
+    @staticmethod
+    def _next_round(data={}):
         """Move to the next round."""
         if self.tournament.current_round_number == self.tournament.N_ROUNDS - 1:
             self.tournament.status = "Completed"
         else:
             self.tournament.current_round_number += 1
-        self.tournament.update()
+        # return "TournamentView.menu", data    self.tournament.update()
 
-
-def play_rounds(tms, tournament):
+    @staticmethod
+    def play_rounds(tms, tournament):
     """Plays rounds for the given tournament."""
+
     if tournament:
         try:
             while tournament.status == "In Progress":
@@ -131,10 +154,10 @@ def play_rounds(tms, tournament):
         except ValueError as e:
             print(e)
     else:
-        print("No tournament available to play rounds.")
+        # return "TournamentView.menu", data    print("No tournament available to play rounds.")
 
-
-def display_available_tournaments(list_tournaments: list) -> str:
+    @staticmethod
+    def display_available_tournaments(list_tournaments: list) -> str:
     """Display a list of available tournaments."""
     if list_tournaments:
         print("\nAvailable Tournaments:")
